3571cb4dfb9eca8adc2dd8c6e459e18f851be690
coderhook <abel1987@hotmail.es>
2019-06-18 15:40:32 +0200

    Changed the tags to classes in navBar to classes to not override the rest of the css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Breeds</a></li>
-            <li style={{float:"right"}}><a href="#" >Play</a></li>
-            <li><a href="">Info</a></li>
+          <ul class="nav">
+            <li class="nav-pill"><a href="#">Home</a></li>
+            <li class="nav-pill"><a href="#">Breeds</a></li>
+            <li style={{float:"right"}} class="nav-pill"><a href="#" >Play</a></li>
+            <li class="nav-pill"><a href="#">Info</a></li>
           </ul>
       </div>
               )

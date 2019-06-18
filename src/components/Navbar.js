23d51bbda286d7456baf8e0affb94e96eeeff035
coderhook <abel1987@hotmail.es>
2019-06-18 16:31:22 +0200

    novabar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,14 @@ import './Navbar.css'
 export default class Navbar extends Component {
   render() {
     return (
-          <ul className="nav">
-            <li className="nav-pill"><a href="#home">Home</a></li>
-            <li className="nav-pill"><a href="#breeds">Breeds</a></li>
-            <li style={{float:"right"}} className="nav-pill"><a href="play" >Play</a></li>
-            <li className="nav-pill"><a href="info">Info</a></li>
+      <div className="Navbar">
+          <ul className="Nav">
+            <li className="Nav-pill"><a href="#home">Home</a></li>
+            <li className="Nav-pill"><a href="#breeds">Breeds</a></li>
+            <li style={{float:"right"}} className="Nav-pill"><a href="#play" >Play</a></li>
+            <li className="Nav-pill"><a href="#info">Info</a></li>
           </ul>
+      </div>
               )
             }
           }

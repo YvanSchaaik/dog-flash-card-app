2212524986fb0ede8dfddbfbf2654644884064df
coderhook <abel1987@hotmail.es>
2019-06-18 16:08:37 +0200

    testing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import './Navbar.css'
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="navbar">
-          <ul class="nav">
-            <li class="nav-pill"><a href="#">Home</a></li>
-            <li class="nav-pill"><a href="#">Breeds</a></li>
-            <li style={{float:"right"}} class="nav-pill"><a href="#" >Play</a></li>
-            <li class="nav-pill"><a href="#">Info</a></li>
-          </ul>
-      </div>
+            <div className="navbar">
+                <ul class="nav">
+                  <li class="nav-pill"><a href="#">Home</a></li>
+                  <li class="nav-pill"><a href="#">Breeds</a></li>
+                  <li style={{float:"right"}} class="nav-pill"><a href="#" >Play</a></li>
+                  <li class="nav-pill"><a href="#">Info</a></li>
+                </ul>
+            </div>
               )
             }
           }

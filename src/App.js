e544db579d61849bc464010d4614771fac10a6e6
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-06-18 15:13:46 +0200

    Connected the redux store with the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import store from './store'
+import {Route} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {DogListContainer} from './component/DogListContainer'
 
 
 class App extends React.Component {
   render(){
     return (
-      <div className="App">
-      </div>
+      <Provider store= {store}>
+        <div className="App">
+          <h1>THISISATEST</h1>
+          <Route exact path ='/dog-breed' component={DogListContainer} />
+        </div>
+      </Provider>
     );
   }
 }

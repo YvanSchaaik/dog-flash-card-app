5c8d4bb7c7fb066662bd72718e514f9c63f75da4
coderhook <abel1987@hotmail.es>
2019-06-18 14:34:17 +0200

    Added navbar pills

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+          <ul>
+            <li><a href="default.asp">Home</a></li>
+            <li><a href="news.asp">Breeds</a></li>
+            <li><a href="contact.asp">Play</a></li>
+            <li><a href="about.asp">Info</a></li>
+          </ul>
+      </div>
+              )
+            }
+          }

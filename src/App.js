b124a155a6333d4f860947a2d323922969ae5d82
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-06-18 17:41:39 +0200

    A working site for now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import DoglistContainer from './components/DoglistContainer'
-import './App.css';
 import store from './store'
 import {Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
-// import {DogListContainer} from './component/DogListContainer'
 import Navbar from './components/Navbar';
 import Home from './components/Home'
 import DogBreedImagesContainer from './components/DogBreedImagesContainer'
 import GamesContainer from './components/GamesContainer';
 import Game1 from './components/games/Game1';
+import DogListContainer from './components/DoglistContainer';
 
 
 
@@ -21,7 +19,9 @@ class App extends React.Component {
         <div className="App">
           <Navbar />
           <Route exact path ='/' component={Home}/>
+          <Route path="dog-list" component={DogListContainer} />
           <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer}/>
+          <Route exact path ='/breeds' component={DogListContainer}/>
           <Route path="/games" component={GamesContainer}/>
           <Route path="/game1" component={Game1}/>
 

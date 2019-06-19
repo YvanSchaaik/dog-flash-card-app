b2f3ea768a14894202f909025f5417d0a42a6343
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-06-19 16:27:07 +0200

    Added a little bit CSS to the DogListTags

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
+import './DogList.css'
 
 
 export default class DogsList extends Component {
@@ -9,7 +10,7 @@ export default class DogsList extends Component {
         <h1>Dogs List</h1>
         {
           this.props.dogBreeds && this.props.dogBreeds.map((breed) =>{
-            return <li key={breed}>
+            return <li className='listNames' key={breed}>
             <Link to={`/${breed}`}>{breed}</Link>
             </li>
           })

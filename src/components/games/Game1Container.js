e6829ea95f890cb7e8de83b4e8af6684b261c9da
coderhook <abel1987@hotmail.es>
2019-06-19 17:52:56 +0200

    Added 3 options to Game1Container with the solution and 2 random options

--- src/components/games/Game1Container.js
+++ src/components/games/Game1Container.js
@@ -11,7 +11,14 @@ export class Game1Container extends Component {
 
   render() {
     if (!this.props.gameOne.solution || !this.props.gameOne.breeds) return 'Loading...'
-    
+
+    const breed = this.props.gameOne.solution.breed
+    const breeds = this.props.gameOne.breeds
+    const random_num = Math.floor(Math.random() * breeds.length )
+
+    const options = [ breed, breeds[random_num], breeds[random_num]]
+
+    console.log('Options', options)
     return (
       <Game1 solution={ this.props.gameOne.solution } />
     )

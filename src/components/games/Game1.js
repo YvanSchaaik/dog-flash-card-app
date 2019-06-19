4d1e40729dd8ca0bf48e1192ebd4335a23053637
coderhook <abel1987@hotmail.es>
2019-06-19 18:06:56 +0200

    Added Game1.css with same image size for every picture

--- src/components/games/Game1.js
+++ src/components/games/Game1.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Game1.css'
 
 export default class Game1 extends Component {
   render() {
@@ -7,7 +8,7 @@ export default class Game1 extends Component {
         <h1>This is the Game #1</h1>
        
         <div>
-        <img src={this.props.solution.image} alt="dog Bree to guess"/>
+        <img className="guessImage" src={this.props.solution.image} alt="dog Bree to guess"/>
         <p>option 1</p>
         <p>{ this.props.solution.breed}</p>
         <p>option 3</p>

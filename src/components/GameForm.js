a2212d6d0331a97fa11e2dcb2f99a9cfc6dcce6e
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-06-20 12:16:05 +0200

    Committing a change that the GameStats can acces the GameForm's state

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class GameForm extends Component {
-    state = {}
+    static propTypes = {
+        addPlayer : PropTypes.func.isRequired
+    }
+
+    state = {
+        name : ''
+    }
 
 handleSubmit = (event) => {
     event.preventDefault()
-    console.log('SUBMITTRIGGERED!')
+    this.props.addPlayer(this.state.name = event.target.value)
+    console.log(`Submitting form creates a ${this.state.name}`)
 }
 
 handleChange = (event) => {
+    this.setState({
+        [event.target.name] : event.target.value
+    })
     console.log('Value form!',event.target.value)
 
 }
 
     render() {
+        console.log( 'test',this.state.name)
         return (
-            <div>
+            <div className="nameForm">
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Name: 
-                        <input type="text" name="title" onChange={this.handleChange} />
+                        <input
+                        type="text"
+                        name="name"
+                        onChange={this.handleChange}
+                        value={this.state.name}
+                        />
+                        
                     </label>
                     <button type="submit">Submit</button>
                 </form>
--- src/components/GameStats.js
+++ src/components/GameStats.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import { GameForm } from './GameForm';
 
 export class GameStats extends Component {
+
+    addPlayer = (name) => {
+        this.setState({
+          players: this.state.name.concat(name)
+        })
+      }
+      
     render() {
         return (
             <div>
             <h1>dummydata</h1>
-            <GameForm />
+            <p>Name: {this.props.name}</p>
+            <p>Score: </p>
+            <p>Percentage: </p>
+            <GameForm addPlayer={this.addPlayer}/>
             </div>
         )
     }

2020130dfe9ae66a131278923bf331ab4beebda1
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-06-20 22:20:52 +0200

    code cleanup in Game3Container

--- src/components/games/Game3Container.js
+++ src/components/games/Game3Container.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux';
-
-import { addDog, guessBreed, getBreeds, getBreedsAndPickThree } from '../../actions/gameTwoActions'
-
 import Game1Container from './Game1Container'
 import Game2Container from './Game2Container'
 
@@ -24,13 +20,9 @@ import Game2Container from './Game2Container'
       gamesPlayed: this.gamesPlayed + 1}  
     )
   }
-
-  componentDidMount = () => {
-    //this.props.getBreedsAndPickThree()
-  }
+  
 
   render() {
-    //if (!this.props.gameTwo.solution || !this.props.gameTwo.breeds) return 'Loading...'
     const renderElement = Math.floor((Math.random() * 2))
     const gameArr = [<Game1Container game3active={true} game3Renew={this.reRender} />,<Game2Container game3active={true} game3Renew={this.reRender} />]
     return (
@@ -40,14 +32,3 @@ import Game2Container from './Game2Container'
     )
   }
 }
-
-// const mapStateToProps = (state) => {
-//   //console.log("MSTP G2 ", state)
-//   return {
-//     gameTwo: state.GameTwo
-//   }
-// }
-
-//export default connect(mapStateToProps, { addDog, guessBreed, getBreeds, getBreedsAndPickThree })(Game2Container)
-
-

89c305a5e200b122909c962c24ef3b8566aca11e
coderhook <abel1987@hotmail.es>
2019-06-20 16:18:51 +0200

    Added percentage of the corect answers of the user

--- src/components/games/Game1.js
+++ src/components/games/Game1.js
@@ -31,13 +31,13 @@ export default class Game1 extends Component {
       <div>
         <h1>This is the Game #1</h1>
         <b>Score</b> Correct: {this.props.score.correct} / wrong: {this.props.score.wrong}
+        <p><b>{ Math.floor(this.props.score.correct/(this.props.score.wrong + this.props.score.correct) * 100) } %</b></p>
         <div>
         <img className="guessImage" src={this.props.solution.image} alt="Dog Breed to guess"/>
         <h3>Solution: {this.props.solution.breed}</h3>
         <div className="optionsButtons">
 
         {
-          
           options.map( (option, index) => <button onClick={() => this.checkAnswer(option)} key={index}> {option} </button>)
         }
 

21e9f74460256eb6b5d3d3dcd19975004a5be538
“YvanSchaaik” <yvanschaaik@gmail.com>
2019-06-20 11:40:10 +0200

    Functions are now returning something in the console

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,10 +4,12 @@ export class GameForm extends Component {
     state = {}
 
 handleSubmit = (event) => {
-
+    event.preventDefault()
+    console.log('SUBMITTRIGGERED!')
 }
 
 handleChange = (event) => {
+    console.log('Value form!',event.target.value)
 
 }
 
@@ -17,8 +19,9 @@ handleChange = (event) => {
                 <form onSubmit={this.handleSubmit}>
                     <label>
                         Name: 
-                        <input type="text" onChange={this.handleChange} />
+                        <input type="text" name="title" onChange={this.handleChange} />
                     </label>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
         )

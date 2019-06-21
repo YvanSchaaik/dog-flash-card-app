ec23cb00ad113daf8e3408b3118a0a8387b0b068
coderhook <abel1987@hotmail.es>
2019-06-21 10:06:11 +0200

    first attempt of hint

--- src/components/games/Game1Container.js
+++ src/components/games/Game1Container.js
@@ -64,7 +64,7 @@ export class Game1Container extends Component {
 
 
     return (
-      <Game1 solution={ this.props.gameOne.solution } options = { options } correct={ (answer_status) => this.nextQuestion(answer_status)} score={this.state.score}/>
+      <Game1 solution={ this.props.gameOne.solution } options = { options } correct={ (answer_status) => this.nextQuestion(answer_status)} score={this.state.score} hint={this.props.gameOne.solution.breed[0]}/>
     )
   }
 }
